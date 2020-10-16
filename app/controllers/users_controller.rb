37fc9fcf296a2160ead732a0ad11250c17258921
github username <github email address>
2020-10-16 11:52:15 -0400

    Auth Complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    end
 
     def index
         users = User.all
@@ -6,12 +11,12 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.create!(user_params)
-
-        if user.valid?
-            render json:{user: UserSerializer.new(@user) }, status :created
+        @user = User.create(user_params)
+        if @user.valid?
+          @token = encode_token(user_id: @user.id)
+          render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
         else
-            render json: {error: user.errors.full_messages}, status: :not_acceptable
+          render json: { error: 'failed to create user' }, status: :not_acceptable
         end
     end
 
